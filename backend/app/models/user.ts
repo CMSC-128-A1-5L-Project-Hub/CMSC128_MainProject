365532e921451eb56214dcb55bfdc5d7dea5da26
jdde19 <estadillajohndavid@gmail.com>
2026-03-05 23:07:17 +0800

    feat: added seeders, added attributes in user table, added provisions, added user model

--- backend/app/models/user.ts
+++ backend/app/models/user.ts
@@ -3,16 +3,41 @@ import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
 import { type AccessToken, DbAccessTokensProvider } from '@adonisjs/auth/access_tokens'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { DateTime } from 'luxon'
 
 export default class User extends compose(UserSchema, withAuthFinder(hash)) {
+
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public first_name: string
+
+  @column()
+  public last_name: string
+
+  @column()
+  public up_mail: string
+
+  @column()
+  public role: string
+
+  @column.dateTime({ autoCreate: true})
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true})
+  public updatedAt: DateTime
+
   static accessTokens = DbAccessTokensProvider.forModel(User)
   declare currentAccessToken?: AccessToken
+    user: Date
 
-  get initials() {
-    const [first, last] = this.fullName ? this.fullName.split(' ') : this.email.split('@')
-    if (first && last) {
-      return `${first.charAt(0)}${last.charAt(0)}`.toUpperCase()
-    }
-    return `${first.slice(0, 2)}`.toUpperCase()
-  }
+  // get initials() {
+  //   const [first, last] = this.fullName ? this.fullName.split(' ') : this.email.split('@')
+  //   if (first && last) {
+  //     return `${first.charAt(0)}${last.charAt(0)}`.toUpperCase()
+  //   }
+  //   return `${first.slice(0, 2)}`.toUpperCase()
+  // }
 }
--- backend/database/migrations/0000000000000_create_users_table.ts
+++ backend/database/migrations/0000000000000_create_users_table.ts
@@ -6,9 +6,10 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
-      table.string('full_name').nullable()
-      table.string('email', 254).notNullable().unique()
-      table.string('password').notNullable()
+      table.string('first_name').nullable()
+      table.string('last_name').nullable()
+      table.string('up_mail').nullable().unique()
+      table.string('role').nullable()
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
