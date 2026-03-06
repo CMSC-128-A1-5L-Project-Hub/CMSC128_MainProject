6a7ce8df7106bd59d202daf6d4d673a383c4c8e4
Joshua <jacarlos2@up.edu.ph>
2026-03-07 00:52:14 +0800

    chore: added commented-out accommodations route

--- backend/.adonisjs/server/routes.d.ts
+++ backend/.adonisjs/server/routes.d.ts
@@ -6,35 +6,26 @@ export type ScannedRoutes = {
   ALL: {
     'auth.redirect': { paramsTuple?: []; params?: {} }
     'auth.callback': { paramsTuple?: []; params?: {} }
-    'auth.new_account.store': { paramsTuple?: []; params?: {} }
-    'auth.access_token.store': { paramsTuple?: []; params?: {} }
-    'auth.access_token.destroy': { paramsTuple?: []; params?: {} }
-    'setups.show': { paramsTuple?: []; params?: {} }
-    'setups.store': { paramsTuple?: []; params?: {} }
     'student_dashboards.index': { paramsTuple?: []; params?: {} }
     'landlord_dashboards.index': { paramsTuple?: []; params?: {} }
-    'profile.profile.show': { paramsTuple?: []; params?: {} }
+    'profile.show': { paramsTuple?: []; params?: {} }
+    'setups.store': { paramsTuple?: []; params?: {} }
   }
   GET: {
     'auth.redirect': { paramsTuple?: []; params?: {} }
     'auth.callback': { paramsTuple?: []; params?: {} }
-    'setups.show': { paramsTuple?: []; params?: {} }
     'student_dashboards.index': { paramsTuple?: []; params?: {} }
     'landlord_dashboards.index': { paramsTuple?: []; params?: {} }
-    'profile.profile.show': { paramsTuple?: []; params?: {} }
+    'profile.show': { paramsTuple?: []; params?: {} }
   }
   HEAD: {
     'auth.redirect': { paramsTuple?: []; params?: {} }
     'auth.callback': { paramsTuple?: []; params?: {} }
-    'setups.show': { paramsTuple?: []; params?: {} }
     'student_dashboards.index': { paramsTuple?: []; params?: {} }
     'landlord_dashboards.index': { paramsTuple?: []; params?: {} }
-    'profile.profile.show': { paramsTuple?: []; params?: {} }
+    'profile.show': { paramsTuple?: []; params?: {} }
   }
   POST: {
-    'auth.new_account.store': { paramsTuple?: []; params?: {} }
-    'auth.access_token.store': { paramsTuple?: []; params?: {} }
-    'auth.access_token.destroy': { paramsTuple?: []; params?: {} }
     'setups.store': { paramsTuple?: []; params?: {} }
   }
 }
