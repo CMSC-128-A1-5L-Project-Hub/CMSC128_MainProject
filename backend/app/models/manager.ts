a4b778c015681f85283cee202ae42cb0e05aafcc
Joshua <jacarlos2@up.edu.ph>
2026-03-08 18:41:16 +0800

    feat: added student,landlord,manager models

---
+++ backend/app/models/manager.ts
@@ -0,0 +1,21 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import User from '#models/user'
+
+export default class Manager extends BaseModel {
+  @column({ isPrimary: true })
+  declare userId: number
+
+  @column()
+  declare status: 'active' | 'inactive'
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+}
