156f56e2969d7e9c8109a587edda9f555de240d9
Joshua <jacarlos2@up.edu.ph>
2026-03-12 12:09:37 +0800

    fix: updated provisioning service, removed created at timestamp (for now)

--- backend/app/services/provisioning_service.ts
+++ backend/app/services/provisioning_service.ts
@@ -5,18 +5,19 @@ export default class ProvisioningService {
    * Accepts a profile object from Google and handles the "Upsert"
    */
   public async provision(profile: { email: string, firstName: string, lastName: string }) {
-    let user
-    try {
-        user = await User
-        .query()
-        .where('email', profile.email) // only checks email if existing because it might still create a new user if the user changes their name
-        .firstOrFail()
-
-        user.updatedAt = new Date()
-        await user.save()
-    } catch (error) {
-        user = await User.create({email: profile.email, first_name: profile.firstName, last_name: profile.lastName, role: 'unassigned'})
+    let user = await User.findBy('email', profile.email)
+    if (user) {
+      user.firstName = profile.firstName
+      user.lastName = profile.lastName
+      await user.save()
+      return user
     }
-    return user
+
+    return await User.create({
+      email: profile.email,
+      firstName: profile.firstName,
+      lastName: profile.lastName,
+      role: 'unassigned'
+    })
   }
 }
