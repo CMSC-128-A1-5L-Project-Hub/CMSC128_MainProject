eacfd9a149ae8a3df7415bba07368acc4c524eec
Liesl Chan <lschan1@up.edu.ph>
2026-03-19 12:37:29 +0800

    fix: updated table names and file names based on updated schema

--- database/applications_crud.sql
+++ database/applications_crud.sql
@@ -1,31 +1,31 @@
 ----- RETRIEVE -----
 -- Retrieve by accommodation_id
-SELECT * FROM application ap
+SELECT * FROM applications ap
 JOIN accommodation ac ON ap.accommodation_id = ac.accommodation_id
 WHERE ap.accommodation_id = 67;
 
 -- Retrieve by student_number
-SELECT * FROM application 
+SELECT * FROM applications
 WHERE student_number = '1234567890';
 
 -- Retrieve by application_date
-SELECT * FROM application 
+SELECT * FROM applications
 WHERE DATE(application_date) = '2067-06-07';
 
 -- Retrieve by application_room_type
-SELECT * FROM application
+SELECT * FROM applications
 WHERE application_room_type = 'single';
 
 -- Retrieve by duration_of_stay_days
-SELECT * FROM application
+SELECT * FROM applications
 WHERE duration_of_stay_days = 3;
 
 -- Retrieve by application_status
-SELECT * FROM application
+SELECT * FROM applications
 WHERE application_status = 'pending';
 
 ----- UPDATE -----
 -- Update application status by application_id
-UPDATE application
+UPDATE applications
 SET application_status = 'approved'
 WHERE application_id = 67;
--- database/rooms_crud.sql
+++ database/rooms_crud.sql
@@ -1,67 +1,67 @@
 ----- RETRIEVE -----
 -- Retrieve by ID
-SELECT * FROM room 
+SELECT * FROM rooms 
 WHERE room_id = 67;
 
 -- Retrieve by tenant_restriction
-SELECT * FROM room
+SELECT * FROM rooms
 WHERE tenant_restriction = 'coed';
 
 -- Retrieve by accommodation_id
-SELECT * FROM room r
+SELECT * FROM rooms r
 JOIN accommodation a ON r.accommodation_id = a.accommodation_id
 WHERE r.accommodation_id = 67;
 
 -- Retrieve by accommodation_id and room_type
-SELECT * FROM room r
+SELECT * FROM rooms r
 JOIN accommodation a ON r.accommodation_id = a.accommodation_id
 WHERE r.accommodation_id = 67
     AND r.room_type = 'shared';
 
 ----- UPDATE -----
 -- Update room number by room_id
-UPDATE room 
+UPDATE rooms 
 SET room_number = '679'
 WHERE room_id = 67; 
 
 -- Update room type by room_id
-UPDATE room 
+UPDATE rooms 
 SET room_type = 'single'
 WHERE room_id = 67; 
 
 -- Update room capacity by room_id
-UPDATE room 
+UPDATE rooms 
 SET room_capacity = 4
 WHERE room_id = 67; 
 
 -- Update room current occupancy by room_id
-UPDATE room 
+UPDATE rooms 
 SET room_current_occupancy = 4
 WHERE room_id = 67; 
 
 -- Update room building by room_id
-UPDATE room 
+UPDATE rooms 
 SET room_building = 'janelle'
 WHERE room_id = 67; 
 
 -- Update room rent by room_id
-UPDATE room 
+UPDATE rooms 
 SET room_rent = 69420.67
 WHERE room_id = 67; 
 
 -- Update room availability by room_id
-UPDATE room 
+UPDATE rooms 
 SET room_availability = 'occupied'
 WHERE room_id = 67; 
 
 -- Update tenant restriction by room_id
-UPDATE room
+UPDATE rooms
 SET tenant_restriction = 'coed'
 WHERE room_id = 67;
 
 ----- DELETE -----
 -- Delete room from table 
-DELETE FROM room 
+DELETE FROM rooms 
 WHERE room_id = 67;
 
 
