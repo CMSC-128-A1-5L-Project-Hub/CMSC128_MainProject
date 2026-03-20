4913c9f28e7b5f89c22ceb8d398c65278d5a8654
Joshua <jacarlos2@up.edu.ph>
2026-03-20 19:08:11 +0800

    chore: added super_admin to constants

--- backend/app/constants/roles.ts
+++ backend/app/constants/roles.ts
@@ -2,6 +2,7 @@ export const ROLES = {
   STUDENT: 'student',
   MANAGER: 'manager',
   LANDLORD: 'landlord',
+  SUPER_ADMIN: 'super_admin'
 } as const
 
 export type Role = (typeof ROLES)[keyof typeof ROLES]
