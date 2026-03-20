3742c33e7048b8bdd62eccea2cb06458d9d9d150
Joshua <jacarlos2@up.edu.ph>
2026-03-20 19:07:43 +0800

    chore: deleted unneeded files

--- backend/app/controllers/profile_controller.ts
+++
@@ -1,9 +0,0 @@
-import type { HttpContext } from '@adonisjs/core/http'
-
-export default class ProfileController {
-  async index({ response }: HttpContext) {
-    return response.json({
-      message: 'Profile endpoint',
-    })
-  }
-}
