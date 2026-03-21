be69f04c97d7b807c275f19f0cb67ce6de819837
DeRamosWR <wederamos@up.edu.ph>
2026-03-21 22:16:51 +0800

    feat: added test pages in frontend for current routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,12 +6,12 @@ import Button from './components/Button.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Button 
+    {/* <Button 
       label="Sign in with Google"
       onClick={() => {
         window.location.href = "http://localhost:3333/auth/google/redirect"
       }} 
-      />
+      /> */}
     <App />
   </StrictMode>,
 )
