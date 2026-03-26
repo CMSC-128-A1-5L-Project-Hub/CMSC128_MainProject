1889a5024f40554d7b85317680414ad27a4881ea
Joy Guevarra <joguevarra5@up.edu.ph>
2026-03-26 21:45:40 +0800

    chore: added initial fees and payments controller

--- backend/app/controllers/payments_controller.ts
+++ backend/app/controllers/payments_controller.ts
@@ -1,5 +1,10 @@
 import type { HttpContext } from '@adonisjs/core/http'
 // Imports: Payment, Fee, LogService, etc.
+import { uploadImage } from '#services/b2_services'
+import Payment from '#models/payment'
+import Fee from '#models/fee'
+import FileMetadata from '#models/file_metadatum'
+import Manager from '#models/manager'
 
 export default class PaymentsController {
   
@@ -8,6 +13,33 @@ export default class PaymentsController {
   async uploadProof({ params, request, response, serialize }: HttpContext) {
     // Logic: Handle file upload to Backblaze.
     // Create new payment row linked to feeId with status 'pending_verification'.
+
+    const feeId = params.feeId
+    const fee = await Fee.findOrFail(feeId)
+
+    const file = request.file('receipt', {
+      size: '5mb',
+      extnames: ['jpg', 'png', 'jpeg', 'pdf'],
+    })!
+
+    await file.moveToDisk('./tmp')
+    const fileUrl = await uploadImage(file, 'payments')
+
+    const fileMeta = await FileMetadata.create({
+      filePath: fileUrl,
+      fileName: file.clientName,
+    })
+
+    const payment = await Payment.create({
+      feeId: fee.id,
+      proofFileId: fileMeta.id,
+      paymentAmount: request.input('paymentAmount')
+    })
+
+    return response.created({
+      message: 'Payment uploaded successfully',
+      payment,
+    })
   }
 
   // ─── MANAGER: VIEW PENDING PAYMENTS ───
@@ -15,6 +47,27 @@ export default class PaymentsController {
   async pending({ auth, serialize }: HttpContext) {
     // Logic: Fetch all payments where status is 'pending_verification' 
     // that belong to accommodations managed by auth.user.
+
+    const user = auth.user!
+    const manager = await Manager
+      .query()
+      .where('userId', user.id)
+      .preload('accommodations')
+      .firstOrFail()
+
+    const accoms = manager.accommodations.map(accom => accom.id)
+    
+    // get fees
+    
+
+    const payments = await Payment
+      .query()
+      .whereIn('feeId', fees.map(fee => fee.id))
+      .andWhere('paymentStatus', 'pending_verification')
+      .preload('fee')
+      .preload('proofFile')
+
+    return serialize(payments)
   }
 
   // ─── MANAGER: APPROVE/REJECT PAYMENT ───
