ba42c9d83bcad893e7e4d3640db6cd6a17eb787b
jssison <jssison5@up.edu.ph>
2026-03-27 11:25:16 +0800

    chore: initial dashboards

---
+++ frontend/src/pages/shared/SignIn.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function SignIn() {
+    return(
+        <div className="min-h-screen flex">
+            {/* Form */}
+            <div className="w-full lg:w-1/2 flex items-center justify-center ">
+
+            </div>
+            {/* Image */}
+            <div>
+
+            </div>
+        </div>
+    )
+}
