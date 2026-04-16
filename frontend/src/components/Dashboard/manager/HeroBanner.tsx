c5acf54b605843e51a0da3020f42c6868003cf67
jssison <jssison5@up.edu.ph>
2026-04-16 12:07:57 +0800

    feat: added reusable hero banner

---
+++ frontend/src/components/Dashboard/manager/HeroBanner.tsx
@@ -0,0 +1,39 @@
+import House from "../../../assets/icons/house_icon.svg"
+
+type HeroProps = {
+    greeting: string
+    name: string
+    title: string
+    pendingApplications: number
+    newNotifications: number
+}
+
+export default function HeroBanner({ 
+    greeting, 
+    name,
+    title,
+    pendingApplications, 
+    newNotifications 
+    }: HeroProps) {
+    return (
+        <div className="rounded-2xl px-7 py-6 flex justify-between items-center overflow-hidden"
+            style={{ background: "linear-gradient(to bottom right, #2A0410 0%, #6B0F2B 50%, #C05070 100%)"}}
+        >
+            <div className="z-10">
+                <p className="text-xs lg:text-sm font-medium tracking-widest uppercase text-[#C9973A]">
+                    {greeting}, {name}
+                </p>
+                <h2 className="text-white font-bold text-lg lg:text-2xl leading-snug">
+                    {title}
+                </h2>
+                <p className="text-xs lg:text-sm tracking-wide text-white/55">
+                    You have {pendingApplications > 0 ? pendingApplications : "no"} pending applications and {newNotifications > 0 ? newNotifications : "no"} new notifications today.
+                </p>
+            </div>
+            <img
+                src={House}
+                className="hidden lg:inline absolute right-[1%] translate-y-[0%] w-56 pointer-events-none"
+            />
+        </div>
+    )
+}
