78f74b80d547a899acaedcebaf9e5058a706f72f
Joshua <jacarlos2@up.edu.ph>
2026-04-19 20:38:11 +0800

    feat: added OTP service and limiter to main_connected. updated .gitignore as well

---
+++ backend/app/controllers/sms_verifications_controller.ts
@@ -0,0 +1,62 @@
+import { inject } from '@adonisjs/core'
+import type { HttpContext } from '@adonisjs/core/http'
+import SmsService from '#services/otp_service'
+import { DateTime } from 'luxon'
+import PhoneNumber from "#models/phone_number"
+
+@inject()
+export default class SmsVerificationsController {
+  constructor(protected smsService: SmsService) {}
+
+  // POST /auth/send-otp
+  async send({ auth, request, response }: HttpContext) {
+    const user = auth.user!
+    
+    // Grab the phone number the user just submitted
+    const { phoneNumber } = request.only(['phoneNumber'])
+
+    // 1. Call the servie to send the otp
+    const otpCode = await this.smsService.sendOtp(phoneNumber)
+
+    // 2. Save the code to the user's database record so we can verify it later
+    user.otpCode = otpCode
+    user.otpExpiresAt = DateTime.now().plus({ minutes: 5 }) // Standard 5 min expiry
+    await user.save()
+
+    return response.ok({
+      message: 'OTP sent successfully',
+    })
+  }
+
+  // POST /auth/verify-sms
+  async verify({ auth, request, response }: HttpContext) {
+    const user = auth.user!
+    const { code, phoneNumber } = request.only(['code', 'phoneNumber'])
+
+    // 1. Check if the code expired
+    if (user.otpExpiresAt && user.otpExpiresAt < DateTime.now()) {
+      return response.badRequest({ message: 'OTP has expired. Please request a new one.' })
+    }
+
+    // 2. Check if the code matches what Semaphore gave us
+    if (user.otpCode !== code) {
+      return response.badRequest({ message: 'Invalid OTP code.' })
+    }
+
+    // 3. Success! Create new phonenumber in the database
+    await PhoneNumber.create({
+      userId: user.id,
+      contactNumber: phoneNumber,
+      isPrimary: true,
+    })
+
+    // Then clear the otpCode and ExpiresAt in users
+    user.otpCode = null
+    user.otpExpiresAt = null
+    await user.save()
+
+    return response.ok({
+      message: 'Phone number verified successfully!',
+    })
+  }
+}
---
+++ frontend/src/pages/shared/RoleSelection.tsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Logo from "../../components/Logo";
+import Button from "../../components/Button";
+import { FaGraduationCap, FaImage } from "react-icons/fa";
+
+export default function RoleSelection() {
+    const [selectedRole, setSelectedRole] = useState("");
+    const navigate = useNavigate();
+
+    const handleRoleSelect = (role: string) => {
+        setSelectedRole(selectedRole === role ? "" : role);
+    };
+
+    const handleSubmit = () => {
+        navigate(`/auth/signup/${selectedRole}`);
+    }
+
+    return (
+        <>
+            <div className="relative flex flex-col items-center justify-center min-h-screen bg-white px-6 sm:px-10 py-10">
+                <div className="absolute top-6 left-6">
+                    <Logo />
+                </div>
+
+                <h1 className="font-sans text-xl lg:text-2xl text-[#1A0008] font-bold">
+                    Select your Role
+                </h1>
+                <p className="font-sans text-[#9A7080] text-sm lg:text-base mb-5">
+                    Let's start tailoring your needs here in{" "}
+                    <span className="italic">UBLE</span>
+                </p>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-5">
+                    {/* Student Card */}
+                    <div
+                        onClick={() => handleRoleSelect("student")}
+                        className={`cursor-pointer flex flex-row items-center border rounded-lg p-6 shadow-lg hover:shadow-xl transition-all
+                                ${selectedRole === "student"
+                                    ? "border-[#9E2040]"
+                                    : null
+                                }
+                            `}
+                    >
+                        <FaGraduationCap className="w-12 h-12 text-[#9E2040] mr-2" />
+                        <div className="flex flex-col">
+                            <h2 className="text-black font-bold text-lg">Student</h2>
+                            <h4 className="text-[#888888] font-semibold -mt-2">
+                                Get to look for an accommodation
+                            </h4>
+                        </div>
+                        
+                    </div>
+                    
+                    {/* Landlord Card */}
+                    <div
+                        onClick={() => handleRoleSelect("landlord")}
+                        className={`cursor-pointer flex flex-row items-center border rounded-lg p-6 shadow-lg hover:shadow-xl transition-all
+                                ${selectedRole === "landlord"
+                                    ? "border-[#9E2040]"
+                                    : null
+                                }
+                            `}
+                    >
+                        <FaImage className="w-12 h-12 text-[#9E2040] mr-2" />
+                        <div className="flex flex-col">
+                            <h2 className="text-black font-bold text-lg">Landlord</h2>
+                            <h4 className="text-[#888888] font-semibold -mt-2">
+                                Create Accommodations
+                            </h4>
+                        </div>
+                        
+                    </div>
+                </div>
+                <Button variant="primary" size="lg" onClick={handleSubmit}>
+                    Continue
+                </Button>
+            </div>
+        </>
+    );
+}
