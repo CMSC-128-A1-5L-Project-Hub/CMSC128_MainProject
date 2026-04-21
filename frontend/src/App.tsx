89885c72580b66fb0932eaf973dfbe73f6c40a8d
Joshua <jacarlos2@up.edu.ph>
2026-04-21 08:18:39 +0800

    chore: removed redundant import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import SignIn from "./pages/shared/SignIn"
 import SignUp from "./pages/shared/SignUp"
