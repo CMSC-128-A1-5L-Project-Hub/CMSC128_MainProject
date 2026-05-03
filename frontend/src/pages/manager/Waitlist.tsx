bcc1f78222fb02f038b0dde8945826fdaf5f71ba
Liesl Chan <lschan1@up.edu.ph>
2026-05-03 15:49:22 +0800

    fix: generalized move in and move out design

--- frontend/src/pages/manager/Waitlist.tsx
+++ frontend/src/pages/manager/Waitlist.tsx
@@ -711,7 +711,7 @@ export default function Waitlist() {
                     title="Waitlist"></CustomHeader>
                 <div className="flex-1 flex flex-col p-4 lg:p-6 lg:gap-6 overflow-y-auto">
                     <main className="flex-1 flex flex-col gap-4 lg:gap-6">
-                        <div className="mt-1">
+                        <div>
                             <HeroBanner 
                                 greeting="Good Day"
                                 name={isLoadingUser ? "Loading..." : isErrorUser ? "Error Loading Name" : user?.fname}
