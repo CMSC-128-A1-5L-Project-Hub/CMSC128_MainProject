128af5cea48fb0efdb366b24a8160f8d88347eb6
jssison <jssison5@up.edu.ph>
2026-05-07 18:13:57 +0800

    refactor: organized dorm seed data

---
+++ backend/database/seed_data/private_dorms_data.ts
@@ -0,0 +1,13 @@
+import { RoomSeedData, DormSeedData } from "./uplb_dorms_data.ts";
+
+/*
+    Based from OSH data -> room inclusions
+*/
+const OSH_BASIC = ["Study desk", "Cabinet", "Laundry Area"]
+const OSH_BASIC_WIFI = ["Study desk", "Cabinet", "Laundry Area", "WiFi Ready"]
+
+
+
+export const PRIVATE_DORMS: DormSeedData[] = [
+    
+]
