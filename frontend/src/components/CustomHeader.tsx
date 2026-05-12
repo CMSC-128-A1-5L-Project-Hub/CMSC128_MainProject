49076c3bef878b6c35347515c026be586c829550
Liesl Chan <lschan1@up.edu.ph>
2026-05-12 16:55:53 +0800

    fix: fixed padding inconsistencies in landlord rooms page

--- frontend/src/components/CustomHeader.tsx
+++ frontend/src/components/CustomHeader.tsx
@@ -1,13 +1,17 @@
 interface HeaderProps{
     title: string;
+    right: React.ReactNode;
 }
 
-export default function Header({title} : HeaderProps) {
+export default function Header({title, right } : { title: string; right?: React.ReactNode }) {
     return (
         <div className="bg-[#F6F2F4]">
-            <div className='flex flex-row justify-start items-center mt-3'>
-                <div className='hidden lg:block w-2 h-6 rounded-xl bg-gradient-to-b ml-5 mr-3 mb-1 from-[#2A0410] via-[#6B0F2B] to-[#C05070]'></div>
-                <h1 className='font-serif font-bold italic text-[32px] lg:text-[33px] text-[#6B0F2B] pl-16 lg:p-0 capitalize'>{title}</h1>
+            <div className='flex flex-row justify-between items-center mt-3'>
+                <div className="flex flex-row justify-start items-center">
+                    <div className='hidden lg:block w-2 h-6 rounded-xl bg-gradient-to-b ml-5 mr-3 mb-1 from-[#2A0410] via-[#6B0F2B] to-[#C05070]'></div>
+                    <h1 className='font-serif font-bold italic text-[32px] lg:text-[33px] text-[#6B0F2B] pl-16 lg:p-0 capitalize'>{title}</h1>
+                </div>
+                {right && <div className="flex items-center gap-2 mr-5">{right}</div>}
             </div>
             <hr className="border-t border-[#6B0F2B] border-opacity-10 mt-1" />
         </div>
