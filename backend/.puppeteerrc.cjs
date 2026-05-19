91293b286645c06eae8537194cdaf3b01c0dbc33
Joshua <jacarlos2@up.edu.ph>
2026-05-19 08:02:21 +0800

    fix: puppeet fix

--- backend/.puppeteerrc.cjs
+++ backend/.puppeteerrc.cjs
@@ -3,4 +3,5 @@ const { join } = require('node:path')
 /** @type {import('puppeteer').Configuration} */
 module.exports = {
   cacheDirectory: join(__dirname, 'node_modules', '.cache', 'puppeteer'),
+  defaultBrowser: 'chrome-headless-shell',
 }
